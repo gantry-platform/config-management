c7fbca8a1fbfcfc0669b8fdce862e03f8071033f
blurblah <blurblah@blurblah.net>
2020-07-27 12:21:37 +0900

    Initial commit

---
+++ src/test/java/ai/gantry/configmanagement/ConfigManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.gantry.configmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
