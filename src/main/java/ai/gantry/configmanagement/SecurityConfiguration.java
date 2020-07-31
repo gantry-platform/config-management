d0023d53024c07ff37149eec03ff3e37d42b5d32
blurblah <blurblah@blurblah.net>
2020-07-31 10:49:46 +0900

    임시로 security 적용

---
+++ src/main/java/ai/gantry/configmanagement/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package ai.gantry.configmanagement;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${temp.auth.id}")
+    private String id;
+    @Value("${temp.auth.password}")
+    private String password;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(id).password(encoder().encode(password)).roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+//                .antMatchers("/zones/**").hasRole("ADMIN")
+                .anyRequest().authenticated().and().httpBasic();
+    }
+
+    @Bean
+    protected PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
